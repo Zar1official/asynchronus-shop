3e77aab38b0dee3d537e4e93ca9f1756fd6ca9c7
Zar <dimzar233445@gmail.com>
2021-09-11 17:35:07 +0300

    added catalog

--- handlers/navigation/__init__.py
+++ handlers/navigation/__init__.py
@@ -1,4 +1,4 @@
 from .navigation import dp
 from .subscribe_mailing import dp
-
+from .catalog import dp
 __all__ = ["dp"]
---
+++ utils/navigation_utils.py
@@ -0,0 +1,26 @@
+from aiogram import Dispatcher
+import asyncio
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+from loader import subscribeDB, shopDB
+
+
+async def send_products(dp: Dispatcher, user_id):
+    products = await shopDB.get_products()
+    basket = []
+    for product in products:
+        caption = (
+            "<b>{0}</b>\n\n{1}\n\n<b>На складе, ед: </b>{2}\n\n<b>Цена "
+            "товара, ₽: </b>{3}".format(
+                product['name'], product['description'], product['count'], product['price']))
+
+        basket.append(asyncio.create_task(dp.bot.send_document(
+            chat_id=user_id,
+            caption=caption,
+            document=product['photo'],
+            reply_markup=InlineKeyboardMarkup().add(
+                InlineKeyboardButton("Купить товар", callback_data=f"buy_product_{product['_id']}")
+            )
+        )))
+    await asyncio.gather(*basket)
+
