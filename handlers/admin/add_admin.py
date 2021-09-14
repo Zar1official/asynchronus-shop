68a42c22b9498957deb39a5280fac2f865b3809f
Zar <dimzar233445@gmail.com>
2021-09-15 00:57:53 +0300

    add admin update

--- handlers/admin/add_admin.py
+++ handlers/admin/add_admin.py
@@ -1,4 +1,5 @@
 from aiogram import types
+from aiogram.dispatcher import FSMContext
 from aiogram.types import CallbackQuery
 from states import AddAdmin
 
@@ -8,10 +9,15 @@ from loader import dp, bot, adminsDB
 @dp.callback_query_handler(text="add_admin")
 async def add_administrator(query: CallbackQuery):
     await bot.delete_message(query.message.chat.id, query.message.message_id)
-    await bot.send_message("Отправьте ссылку на профиль пользователя телеграм")
+    await bot.send_message(query.from_user.id, "Отправьте id человека, которого нужно сделать админом."
+                           " User-id можно узнать здесь @get_any_telegram_id_bot")
     await AddAdmin.confirm.set()
 
 
-# @dp.message_handler(state=AddAdmin.confirm)
-# async def confirm_adding_admin(message: types.Message):
-#
+@dp.message_handler(state=AddAdmin.confirm)
+async def confirm_adding_admin(message: types.Message, state: FSMContext):
+    await adminsDB.add_admin(int(message.text))
+    await message.answer("Добавлен новый администратор!")
+    await state.finish()
+
+
