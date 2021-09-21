953b86fde41e9e270b8b23b310ffe58a477fc14f
Zar <dimzar233445@gmail.com>
2021-09-21 03:41:08 +0300

    update

--- utils/basket_utils.py
+++ utils/basket_utils.py
@@ -11,7 +11,7 @@ async def send_basket(user_id, dp: Dispatcher):
     names = ""
     for doc in basket_data:
         total_amount += doc['product_price'] * doc['product_count']
-        names += f"{doc['product_name']}: {doc['product_count']}, "
+        names += f"<b>{doc['product_name']}</b>: {doc['product_count']}, "
     await dp.bot.send_message(user_id, names + '\n\nОбщая сумма покупки: {0}'.format(total_amount),
                               reply_markup=InlineKeyboardMarkup().add(
                                   InlineKeyboardButton("Оформить заказ", callback_data=f"buy")
