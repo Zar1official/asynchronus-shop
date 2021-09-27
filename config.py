6ccc46f47c6b5df9af6c2a3b5d9c8bd088709ce3
Zar <dimzar233445@gmail.com>
2021-09-27 18:49:56 +0300

    config updated

--- config.py
+++ config.py
@@ -1,17 +1,20 @@
-TOKEN = "YOUR_BOT_TOKEN"
+TOKEN = "BOT_TOKEN"
+YOO_TOKEN = "YOOKASSA_TOKEN"
+
 USER_NAME = "user_name"
-USER_PASSWORD = "password"
+USER_PASSWORD = "user_password"
 CLUSTER_NAME = "Cluster0"
 
 SHOP_DB_NAME = "products"
+BASKET_DB_NAME = "basket"
+USERS_DB_NAME = "users"
+
+BASKET_COLLECTION = "customers"
+SUBSCRIBE_COLLECTION = "subscribers"
+ADMINS_COLLECTION = "admins"
 SHOP_DB_COLLECTION = "catalog"
 
-SUBSCRIBE_DB_NAME = "users"
-SUBSCRIBE_DB_COLLECTION = "subscribers"
 
 MONGO_KEY_1 = f"mongodb+srv://{USER_NAME}:{USER_PASSWORD}@{CLUSTER_NAME}.nyjf3.mongodb.net/{SHOP_DB_NAME}?retryWrites=true&w=majority"
-MONGO_KEY_2 = f"mongodb+srv://{USER_NAME}:{USER_PASSWORD}@{CLUSTER_NAME}.nyjf3.mongodb.net/{SUBSCRIBE_DB_NAME}?retryWrites=true&w=majority"
-
-ADMIN_IDS = {"YOUR_TELEGRAM_ID"}
-
-
+MONGO_KEY_2 = f"mongodb+srv://{USER_NAME}:{USER_PASSWORD}@{CLUSTER_NAME}.nyjf3.mongodb.net/{USERS_DB_NAME}?retryWrites=true&w=majority"
+MONGO_KEY_3 = f"mongodb+srv://{USER_NAME}:{USER_PASSWORD}@{CLUSTER_NAME}.nyjf3.mongodb.net/{BASKET_DB_NAME}?retryWrites=true&w=majority"
